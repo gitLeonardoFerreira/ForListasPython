72ec68ceb5c9a4534a5a1fce9c833f672224badb
Leonardo <leomurilobarbosa@gmail.com>
2026-04-28 11:31:30 -0300

    exercicios de For

---
+++ exFOR.py
@@ -0,0 +1,33 @@
+# exercicio 1
+
+"""
+for numero in range(1,51):
+    print(numero)
+"""
+
+# exerccio 2
+
+"""
+for numero in reversed(range(100, 201)): #reversed exibe o contrario
+    print(numero)
+"""
+
+"""
+for numero in range(200, 99, -1):
+    print(numero)
+"""
+
+# exercicio 3
+
+"""
+for numero in range(1,101):
+    if numero % 2 != 0:
+        print(numero)
+"""
+# exercicio 4
+
+"""
+import random #biblioteca
+for i in range(1,21):
+    print(random.randint(1,50), end= ' ')
+"""
